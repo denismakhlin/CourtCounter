5885bdc55a1287e58aa28828f82f32f5c0946cec
Denis Makhlin <denismakhlin@gmail.com>
2016-10-27 14:02:18 -0700

    working on milestone 2

--- app/src/main/java/android/makhlind/courtcounter/MainActivity.java
+++ app/src/main/java/android/makhlind/courtcounter/MainActivity.java
@@ -2,6 +2,11 @@ package android.makhlind.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+    private TextView textViewVariable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewVariable = (TextView)findViewById(R.id.textViewName);
+    }
+
+    public void clickFunction1(View view){
     }
 }
+
+
+
+
+
