0870fa0361a2c429cd75595e4b33edd4d892c986
Denis Makhlin <denismakhlin@gmail.com>
2016-11-14 14:17:37 -0800

    DONE

--- app/src/main/java/android/makhlind/courtcounter/MainActivity.java
+++ app/src/main/java/android/makhlind/courtcounter/MainActivity.java
@@ -1,31 +1,112 @@
 package android.makhlind.courtcounter;
-import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.media.MediaPlayer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView dennisView3;
-    private int text3;
-    private TextView dennisView6;
-    private int a;
+    private int denisTeam = 0;
+    private int otherTeam = 0;
+    EditText teamName1View;
+    EditText teamName2View;
 
+    AlertDialog gameOverAlert;
+
+    private MediaPlayer mediaPlayer;
+    public String team1Name;
+    public String team2Name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dennisView3 = (TextView)findViewById(R.id.dennisView3);
-        text3=0;
-        dennisView3.setText("0");
-        dennisView6 = (TextView)findViewById(R.id.dennisView6);
-        dennisView6.setText("0");
+        teamName1View = (EditText) findViewById(R.id.denisedit1);
+        teamName2View = (EditText) findViewById(R.id.denisedit2);
     }
-    public void textClick3(View view){
-        text3++;
-        dennisView3.setText("" + text3);
+
+    public void displayForTeamDenis(int score) {
+        TextView denisTeam = (TextView) findViewById(R.id.denisedit1);
+        if (denisTeam != null) {
+            denisTeam.setText(String.valueOf(score));
+        }
+    }
+
+    public void displayForTeamOther(int score) {
+        TextView otherTeam = (TextView) findViewById(R.id.denisedit2);
+        if (otherTeam != null) {
+            otherTeam.setText(String.valueOf(score));
+        }
+    }
+
+    public void addOneForTeamDenis(View view) {
+        int step = 1;
+        denisTeam += step;
+        mediaPlayer = MediaPlayer.create(this, R.raw.nice);
+        mediaPlayer.start();
+        displayForTeamDenis(denisTeam);
+    }
+
+    public void subtractOneForDenis(View view) {
+        int step = 1;
+        denisTeam -= step;
+        displayForTeamDenis(denisTeam);
+    }
+
+    public void addOneForTeamOther(View view) {
+        int step = 1;
+        otherTeam += step;
+        displayForTeamOther(otherTeam);
+    }
+
+    public void subtractOneForTeamOther(View view) {
+        int step = 1;
+        otherTeam -= step;
+        displayForTeamOther(otherTeam);
+    }
+
+    public void resetScore(View view) {
+        mediaPlayer.start();
+        gameOverAlert = new AlertDialog.Builder(MainActivity.this).create();
+        String team1Name = teamName1View.getText().toString();
+        String team2Name = teamName2View.getText().toString();
+        gameOverAlert.setTitle("Game Over");
+
+        gameOverAlert.setMessage("Game Over");
+
+        if (denisTeam > otherTeam) {
+            gameOverAlert.setMessage(team1Name + "wins");
+        }
+
+        if (denisTeam < otherTeam) {
+            gameOverAlert.setMessage(team2Name + "wins");
+
+        }
+
+
+        if (denisTeam == otherTeam) {
+            gameOverAlert.setMessage("It's a draw");
+        }
+
+        gameOverAlert.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        gameOverAlert.show();
+        denisTeam = 0;
+        otherTeam = 0;
+        displayForTeamDenis(denisTeam);
+        displayForTeamOther(otherTeam);
+
+
     }
 }
+
+
+
